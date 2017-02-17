b1d9ace96453b8868477cb5ca705d2c691b0cd36
KshitijBahul <chunks.3@gmail.com>
2017-02-17 10:58:01 +0530

    Integrated custom Circle Scripts

---
+++ deployment-dev.sh
@@ -0,0 +1,8 @@
+#Declaring the AWS Host Name
+AWS_SSH_HOST="ubuntu@ec2-54-169-211-235.ap-southeast-1.compute.amazonaws.com"
+AWS_CREDENTIAL_FILE="milescampawskeypair.pem"
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub |ssh -i $AWS_CREDENTIAL_FILE $AWS_SSH_HOST 'cat >> .ssh/authorized_keys'
+ssh $AWS_SSH_HOST "pkill -9 java && sudo rm /home/ubuntu/SpringBootPoc.jar"
+scp target/SpringBootPoc.jar $AWS_SSH_HOST:/home/ubuntu
+ssh $AWS_SSH_HOST "java -jar SpringBootPoc.jar"
