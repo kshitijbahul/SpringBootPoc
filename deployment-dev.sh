0f61851f3defec519192b367b7448ecd0104da0b
KshitijBahul <chunks.3@gmail.com>
2017-02-17 13:44:52 +0530

    Test AWS ring

--- deployment-dev.sh
+++ deployment-dev.sh
@@ -1,5 +1,5 @@
 #Declaring the AWS Host Name
-AWS_SSH_HOST="ubuntu@ec2-54-169-211-235.ap-southeast-1.compute.amazonaws.com"
+AWS_SSH_HOST="ubuntu@ec2-54-179-133-202.ap-southeast-1.compute.amazonaws.com"
 AWS_CREDENTIAL_FILE="milescampawskeypair.pem"
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub |ssh -i $AWS_CREDENTIAL_FILE $AWS_SSH_HOST 'cat >> .ssh/authorized_keys'
