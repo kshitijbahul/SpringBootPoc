7dbafa2525293958be92769ec9efc2ad826e357f
Kshitij Bahul <chunkks.3@gmail.com>
2017-05-17 15:36:10 +0530

    Integrated and tested gulp sass conversion

--- src/main/resources/static/gulpfile.js
+++ src/main/resources/static/gulpfile.js
@@ -1,16 +1,28 @@
-//'use strict;'
+'use strict;'
 //const gulp = require('gulp');
 //const devBuild = (process.env.NODE_ENV !== 'production');
-//const folder = {
-//    src: 'src/main/resources/static',
-//    build: 'build/'
-//  };
+const folder = {
+    src: 'src/main/resources/static',
+    build: 'build/'
+};
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var path = require('path');
 
-gulp.task('default', function() {
+gulp.task('sass', function(){
+  console.log('In the css task',folder.src,path.join(__dirname,'/scss/**/*.scss'));
+  return gulp.src(path.join(__dirname,'/scss/**/*.scss'))
+      .pipe(sass().on('error',sass.logError))
+      .pipe(gulp.dest(path.join(__dirname,'/css')));
+});
+
+
+
+gulp.task('testGulp', function() {
   if(process.env.NODE_ENV === 'production') {
     console.log('gulp runs as expected on production');
   } else {
     console.log('gulp runs as expected');
   }
 });
+gulp.task('default',['testGulp','sass']);
